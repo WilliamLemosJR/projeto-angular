b56da3a54c0a536f163d6281eb198b1f99eb4ed0
WilliamLemosJR <127670993+WilliamLemosJR@users.noreply.github.com>
2023-10-05 16:08:18 -0300

    atualizando

---
+++ projeto/src/app/my-componet/my-componet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-componet',
+  templateUrl: './my-componet.component.html',
+  styleUrls: ['./my-componet.component.scss']
+})
+export class MyComponetComponent {
+
+}
